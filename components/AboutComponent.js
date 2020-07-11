ba8acc01700bc2392a68fa18c7ed264bbfd400c4
Hung Le <hple2002@gmail.com>
2020-07-10 20:25:33 -0400

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -17,7 +18,6 @@ class About extends Component {
     };
 
     render() {
-
         const renderPartner = ({item}) => {
             return (
                 <ListItem
@@ -28,18 +28,31 @@ class About extends Component {
             );
         };
 
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
         return (
             <ScrollView>
-
-
-                <Card title='Our Mission' wrapperStyle={{margin: 10}}>
-
-                    <Text>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
-
-
-
-                </Card>
-
+                <Mission />
                 <Card title='Community Partners' wrapperStyle={{margin: 10}}>
                         <FlatList
                             data={this.props.partners.partners}
@@ -47,8 +60,6 @@ class About extends Component {
                             keyExtractor={item => item.id.toString()}
                         />
                 </Card>
-
-
             </ScrollView>
         );
     }
