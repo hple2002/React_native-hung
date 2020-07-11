88e2b38640ed72babfb93eba383c60f55548d802
Hung Le <hple2002@gmail.com>
2020-07-10 21:46:21 -0400

    Forms and mission added

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -20,16 +20,14 @@ class Contact extends Component {
         return (
             <ScrollView>
 
-                <Card title='Contact Information' wrapperStyle={{margin: 10}}
-                >
-                    {/* <View> */}
-                    {/* <Text style={{ marginBottom: 20, textAlign:'center', fontWeight: 'bold' }}>Contact Information</Text> */}
+                <Card title='Contact Information' wrapperStyle={{margin: 10}}>
+
                     <Text>1 Nucamp Way </Text>
                     <Text>Seattle, WA 98001</Text>
                     <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
                     <Text>Phone: 1-206-555-1234</Text>
                     <Text>Email: campsites@nucamp.co</Text>
-                    {/* </View> */}
+
 
                 </Card>
 
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import Contact from './ContactComponent';
+import Reservation from './ReservationComponent';
 import About from './AboutComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
@@ -13,12 +14,17 @@ import { connect } from 'react-redux';
 import { fetchCampsites, fetchComments, fetchPromotions,
     fetchPartners } from '../redux/ActionCreators';
 
+
+
+
 const mapDispatchToProps = {
     fetchCampsites,
     fetchComments,
     fetchPromotions,
     fetchPartners
 };
+
+
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -118,6 +124,29 @@ const ContactNavigator = createStackNavigator(
     }
 );
 
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation }
+    },
+    {
+        navigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView 
@@ -165,6 +194,21 @@ const MainNavigator = createDrawerNavigator(
                 )
             }
         },
+        
+        Reservation: {
+            screen: ReservationNavigator,
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='tree'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
         About: {
             screen: AboutNavigator,
             navigationOptions: {
@@ -193,6 +237,7 @@ const MainNavigator = createDrawerNavigator(
                 )
             }
         }
+        
     },
     {
         drawerBackgroundColor: '#CEC8FF',
